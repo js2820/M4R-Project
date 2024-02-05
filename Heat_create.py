6dff6432d97c5bd5e8ff287c583073729fc5676c
js2820 <js2820@ic.ac.uk>
2024-02-05 16:51:57 +0000

    Added paradiag code

--- Heat_create.py
+++ Heat_create.py
@@ -1,5 +1,11 @@
 from firedrake import *
-mesh = UnitSquareMesh(10, 10, name = 'meshA')
+
+ncells = 20
+base_mesh = PeriodicUnitIntervalMesh(ncells)
+
+height = 1.
+nlayers = 10
+mesh = ExtrudedMesh(base_mesh, layers = nlayers, layer_height=height/nlayers, name = 'meshA')
 
 V = FunctionSpace(mesh, "CG", 1)
 
@@ -9,23 +15,39 @@ v = TestFunction(V)
 f = Function(V)
 
 deltat = 0.1
-time = [deltat*i for i in range(int(1/deltat) + 1)]
+
+t = 0
 
 x, y = SpatialCoordinate(mesh)
 
-f.interpolate(sin(2*pi*x)*cos(pi*x))
+kappa = Constant(0.1)
+C = Constant(1)
 
-LHS = (inner(v, u) + deltat*inner(v.dx(0), u.dx(0)))*dx
+f.interpolate(sin(2*pi*x)*cos(pi*x))
+u0 = Function(V).assign(f)
+
+
+LHS = (inner(v, u) + kappa*deltat*inner(v.dx(0), u.dx(0)) + deltat*C*v*u.dx(0))*dx
 
 RHS = inner(v, f) * dx
 
 u = Function(V, name = 'u')
 
-for t in time:
-    solve(LHS == RHS, u, solver_parameters={'ksp_type': 'cg', 'pc_type': 'none'})
-    f = u
-    RHS = inner(v, f) * dx
+while t < (1-deltat/2):
+    solve(LHS == RHS, u, solver_parameters={'ksp_type': 'preonly', 'pc_type': 'lu'})
+    f.assign(u)
+    t+=deltat
+
+pcg = PCG64(seed=123456789)
+rg = RandomGenerator(pcg)
+
+f_beta = rg.normal(V, 0, 0.1)
+#u += f_beta
+
+print('New')
 
 with CheckpointFile("heat.h5", 'w') as afile:
     afile.save_mesh(mesh)  # optional
     afile.save_function(u)
+
+File("weak1creat.pvd").write(u, u0)
--- Paradiag.py
+++ Paradiag.py
@@ -14,15 +14,18 @@ VP = FunctionSpace(mesh, Pelt)
 W = VU * VP # the mixed space for the coupled x-p system
 
 # solution!
-w = Function(W)
-u, p = split(w)
+wk = Function(W)
+uk, pk = split(wk)
+
+wkp1 = Function(W)
+ukp1, pkp1 = split(wkp1)
 
 # model parameters
 # initial condition from before seeing the data
 x, t = SpatialCoordinate(mesh)
 u0 = sin(2*pi*x)*cos(pi*x)
 # gamma - variance in initial condition error
-gamma = Constant(0.1)
+gamma = Constant(1)
 # variance in observation error
 C = Constant(0.1)
 # fake some data
@@ -35,33 +38,41 @@ c = Constant(1)
 # the functional F
 
 # penalty term for initial condition (f0)
-F = 0.5*(u-u0)*(u-u0)/gamma*ds_b # integral over t=0 "surface"
+F = 0.5*(ukp1-u0)*(ukp1-u0)/gamma*ds_b # integral over t=0 "surface"
 
 # "data" (g0)
-F += 0.5*(u-u1)*(u-u1)/C*ds_t # integral over t=1 "surface"
- 
+F += 0.5*(ukp1-u1)*(ukp1-u1)/C*ds_t # integral over t=1 "surface"
+
 # "cost"
-F += -0.5*p*p/Sigma*dx
+F += -0.5*pkp1*pkp1/Sigma*dx
 
 # "dynamical constraint"
-F += (p*u.dx(1) + kappa*p.dx(0)*u.dx(0) + c*v*u.dx(0))*dx
+F += (pkp1*ukp1.dx(1) + kappa*pkp1.dx(0)*ukp1.dx(0) + c*pkp1*ukp1.dx(0))*dx
 
 # the equation to solve
-dF = derivative(F, w)
+dF = derivative(F, wkp1)
 
 # make a problem to solve
 daparams = {
     "ksp_type": "preonly",
     "pc_type": "lu",
     "pc_factor_mat_solver_type": "mumps"
-}
+} 
 
-DAProblem = NonlinearVariationalProblem(dF, w)
+boundary = DirichletBC(W.sub(0), u0 - gamma*pk, 'bottom')
+DAProblem = NonlinearVariationalProblem(dF, wkp1, bcs = boundary)
 DASolver = NonlinearVariationalSolver(DAProblem,
                                       solver_parameters=daparams)
-DASolver.solve()
 
-u, p = w.subfunctions
+u, p = wkp1.subfunctions
+outfile = File("Para.pvd")
+outfile.write(u, p)
 
-File("weak1.pvd").write(u, p)
+for i in range(10):
+    DASolver.solve()
+    wk.assign(wkp1)
+    outfile.write(u, p)
+
+
+
 #Write the solutions to another file
