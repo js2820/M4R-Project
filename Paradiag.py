f8d3a94ece49b8ba5dc0a3d26eca225decddfcd4
js2820 <js2820@ic.ac.uk>
2024-02-12 11:49:54 +0000

    Diverginfg!!?

--- Paradiag.py
+++ Paradiag.py
@@ -31,10 +31,10 @@ C = Constant(0.1)
 # fake some data
 u1 = 0.5*sin(2*pi*x)
 # variance for model error
-Sigma = Constant(0.0001)
+Sigma = Constant(0.1)
 
 kappa = Constant(0.1)
-c = Constant(1)
+c = Constant(0.1)
 # the functional F
 
 # penalty term for initial condition (f0)
@@ -52,27 +52,53 @@ F += (pkp1*ukp1.dx(1) + kappa*pkp1.dx(0)*ukp1.dx(0) + c*pkp1*ukp1.dx(0))*dx
 # the equation to solve
 dF = derivative(F, wkp1)
 
-# make a problem to solve
-daparams = {
+daparams_brute = {
     "ksp_type": "preonly",
     "pc_type": "lu",
-    "pc_factor_mat_solver_type": "mumps"
+    "pc_factor_mat_solver_type": "mumps",
+    'snes_monitor' : None,
+    'ksp_monitor' : None
+}
+
+# make a problem to solve
+daparams_field = {
+    "ksp_type": "gmres",
+    "pc_type": "fieldsplit",
+    "pc_fieldsplit_type" : "additive",
+    'fieldsplit_0_ksp_type' : 'preonly',
+    'fieldsplit_0_pc_type' : 'lu',
+    'fieldsplit_0_pc_factor_mat_solver_type' : 'mumps',
+    'fieldsplit_1_ksp_type' : 'preonly',
+    'fieldsplit_1_pc_type' : 'lu',
+    'fieldsplit_1_pc_factor_mat_solver_type' : 'mumps',
+    'snes_monitor' : None,
+    'ksp_monitor' : None,
+    'fieldsplit_0_ksp_monitor' : None,
+    'fieldsplit_1_ksp_monitor' : None,
 } 
 
 boundary = DirichletBC(W.sub(0), u0 - gamma*pk, 'bottom')
 DAProblem = NonlinearVariationalProblem(dF, wkp1, bcs = boundary)
 DASolver = NonlinearVariationalSolver(DAProblem,
-                                      solver_parameters=daparams)
+                                      solver_parameters=daparams_field)
 
 u, p = wkp1.subfunctions
 outfile = File("Para.pvd")
 outfile.write(u, p)
 
-for i in range(10):
+error = 1e10
+tol = 1e-4
+count = 0
+alpha = 0.5
+
+while error > tol:
+    count += 1
     DASolver.solve()
-    wk.assign(wkp1)
+    error = errornorm(wk, wkp1)
+    
+    wk.assign(alpha*wk + (1-alpha)*wkp1)
     outfile.write(u, p)
 
-
+    print(count)
 
 #Write the solutions to another file
